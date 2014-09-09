698001496f39f1949129b50cbec2fbb699ab6ebd
Chris Nelson <cnelson87@gmail.com>
2014-09-09 11:06:27 -0700

    html & css updates

--- grunt_tasks/concat.js
+++ grunt_tasks/concat.js
@@ -19,7 +19,8 @@ module.exports = function (grunt) {
 				'<%= sourceVendor %>/jquery.js',
 				'<%= sourceVendor %>/greensock.js',
 				'<%= sourceVendor %>/lodash.js',
-				'<%= sourceVendor %>/bootstrap.js'
+				'<%= sourceVendor %>/bootstrap.js',
+				'<%= sourceVendor %>/bootstrap-datepicker.js'
 			],
 			dest: '<%= localScripts %>/vendor.js'
 		},
@@ -30,7 +31,8 @@ module.exports = function (grunt) {
 				'<%= sourceVendor %>/jquery.js',
 				'<%= sourceVendor %>/greensock.js',
 				'<%= sourceVendor %>/lodash.js',
-				'<%= sourceVendor %>/bootstrap.js'
+				'<%= sourceVendor %>/bootstrap.js',
+				'<%= sourceVendor %>/bootstrap-datepicker.js'
 			],
 			dest: '<%= publicScripts %>/vendor.js'
 		}
